48e465f8579668cb36db2260872afc59ed29a049
Alexandr <yaromahob@gmail.com>
2021-10-04 00:16:45 +0300

    feat: solved task what-season, dream-team, count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,7 +14,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function countCats(matrix) {
+  let counter = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      if (matrix[i][k] == "^^") {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
 }
--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,48 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (date === undefined) {
+    return "Unable to determine the time of year!";
+  }
+
+  try {
+    date.getUTCDate();
+  } catch (error) {
+    throw new Error("Invalid date!");
+  }
+
+  if (isValidDate(date) === false) {
+    throw new Error("Invalid date!");
+  }
+  const monthSeason = [
+    "winter",
+    "winter",
+    "spring",
+    "spring",
+    "spring",
+    "summer",
+    "summer",
+    "summer",
+    "autumn",
+    "autumn",
+    "autumn",
+    "winter",
+  ];
+
+  function isValidDate(date) {
+    return date instanceof Date && !isNaN(date);
+  }
+
+  return monthSeason[date.getMonth()];
 }

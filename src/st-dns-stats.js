79045f845881196bd1a0477e6e68546c0b31dc0d
Alexandr <yaromahob@gmail.com>
2021-10-04 21:40:35 +0300

    feat: add task

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,7 +22,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  let finishObj = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    let dnsName = "";
+    let arr = domains[i].split(".").reverse();
+
+    for (let j = 0; j < arr.length; j++) {
+      dnsName += `.${arr[j]}`;
+
+      if (dnsName in finishObj) {
+        finishObj[dnsName] += 1;
+      } else {
+        finishObj[dnsName] = 1;
+      }
+    }
+  }
+
+  return finishObj;
 }
--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +10,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let encoded = [];
+  let counter = 1;
+  let arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == arr[i + 1]) {
+      counter++;
+    } else {
+      if (counter > 1) {
+        encoded.push(counter);
+      }
+      encoded.push(arr[i]);
+      counter = 1;
+    }
+  }
+
+  return encoded.join("");
 }
